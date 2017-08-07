aef37b63557eeab8cad0a46ca305454eabf3ba88
itchenyi <itchenyi@gmail.com>
2017-08-07 10:07:31 +0800

    Optimize the completion signal

--- gpool.go
+++ gpool.go
@@ -2,26 +2,26 @@ package gpool
 
 import "sync"
 
+// Gorouting instance which can accept client jobs
 type worker struct {
 	workerPool chan *worker
 	jobChannel chan Job
-	stop       chan bool
+	stop       chan struct{}
 }
 
 func (w *worker) start() {
 	go func() {
 		var job Job
 		for {
+			// worker free, add it to pool
 			w.workerPool <- w
 
 			select {
 			case job = <-w.jobChannel:
 				job()
-			case stop := <-w.stop:
-				if stop {
-					w.stop <- true
-					return
-				}
+			case <-w.stop:
+				w.stop <- struct{}{}
+				return
 			}
 		}
 	}()
@@ -31,14 +31,15 @@ func newWorker(pool chan *worker) *worker {
 	return &worker{
 		workerPool: pool,
 		jobChannel: make(chan Job),
-		stop:       make(chan bool),
+		stop:       make(chan struct{}),
 	}
 }
 
+// Accepts jobs from clients, and waits for first free worker to deliver job
 type dispatcher struct {
 	workerPool chan *worker
 	jobQueue   chan Job
-	stop       chan bool
+	stop       chan struct{}
 }
 
 func (d *dispatcher) dispatch() {
@@ -47,18 +48,16 @@ func (d *dispatcher) dispatch() {
 		case job := <-d.jobQueue:
 			worker := <-d.workerPool
 			worker.jobChannel <- job
-		case stop := <-d.stop:
-			if stop {
-				for i := 0; i < cap(d.workerPool); i++ {
-					worker := <-d.workerPool
+		case <-d.stop:
+			for i := 0; i < cap(d.workerPool); i++ {
+				worker := <-d.workerPool
 
-					worker.stop <- true
-					<-worker.stop
-				}
-
-				d.stop <- true
-				return
+				worker.stop <- struct{}{}
+				<-worker.stop
 			}
+
+			d.stop <- struct{}{}
+			return
 		}
 	}
 }
@@ -67,7 +66,7 @@ func newDispatcher(workerPool chan *worker, jobQueue chan Job) *dispatcher {
 	d := &dispatcher{
 		workerPool: workerPool,
 		jobQueue:   jobQueue,
-		stop:       make(chan bool),
+		stop:       make(chan struct{}),
 	}
 
 	for i := 0; i < cap(d.workerPool); i++ {
@@ -79,6 +78,7 @@ func newDispatcher(workerPool chan *worker, jobQueue chan Job) *dispatcher {
 	return d
 }
 
+// Represents user request, function which should be executed in some worker.
 type Job func()
 
 type Pool struct {
@@ -87,6 +87,11 @@ type Pool struct {
 	wg         sync.WaitGroup
 }
 
+// Will make pool of gorouting workers.
+// numWorkers - how many workers will be created for this pool
+// queueLen - how many jobs can we accept until we block
+//
+// Returned object contains JobQueue reference, which you can use to send job to pool.
 func NewPool(numWorkers int, jobQueueLen int) *Pool {
 	jobQueue := make(chan Job, jobQueueLen)
 	workerPool := make(chan *worker, numWorkers)
@@ -99,19 +104,28 @@ func NewPool(numWorkers int, jobQueueLen int) *Pool {
 	return pool
 }
 
+// In case you are using WaitAll fn, you should call this method
+// every time your job is done.
+//
+// If you are not using WaitAll then we assume you have your own way of synchronizing.
 func (p *Pool) JobDone() {
 	p.wg.Done()
 }
 
+// How many jobs we should wait when calling WaitAll.
+// It is using WaitGroup Add/Done/Wait
 func (p *Pool) WaitCount(count int) {
 	p.wg.Add(count)
 }
 
+// Will wait for all jobs to finish.
 func (p *Pool) WaitAll() {
 	p.wg.Wait()
 }
 
+// Will release resources used by pool
 func (p *Pool) Release() {
-	p.dispatcher.stop <- true
+	p.dispatcher.stop <- struct{}{}
 	<-p.dispatcher.stop
 }
+
